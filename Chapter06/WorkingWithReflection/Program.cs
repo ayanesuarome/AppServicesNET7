c53d624d2250de205d0d6f4f3b2ee8b3cf3f331a
Ayane Suarez Romero <Ayane.Suarez@external.wolterskluwer.com>
2025-06-27 16:47:55 +0200

    Chapter 06

---
+++ Chapter06/WorkingWithReflection/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Reflection;
+using WorkingWithReflection;
+
+WriteLine("Assembly metadata:");
+Assembly? assembly = Assembly.GetEntryAssembly();
+
+if (assembly is null)
+{
+    WriteLine("Failed to get entry assembly.");
+    return;
+}
+
+WriteLine($" Full name: {assembly.FullName}");
+WriteLine($" Location: {assembly.Location}");
+WriteLine($" Entry point: {assembly.EntryPoint?.Name}");
+IEnumerable<Attribute> attributes = assembly.GetCustomAttributes();
+WriteLine($" Assembly-level attributes:");
+foreach (Attribute a in attributes)
+{
+    WriteLine($" {a.GetType()}");
+}
+
+AssemblyInformationalVersionAttribute? version = assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>();
+WriteLine($" Version: {version?.InformationalVersion}");
+
+AssemblyCompanyAttribute? company = assembly.GetCustomAttribute<AssemblyCompanyAttribute>();
+WriteLine($" Company: {company?.Company}"); // = WorkingWithReflection by defautl. Apps and Services with .NET -> see project definition
+
+
+WriteLine();
+WriteLine($"* Types:");
+Type[] types = assembly.GetTypes();
+
+foreach (Type type in types)
+{
+    WriteLine();
+    WriteLine($"Type: {type.FullName}");
+    MemberInfo[] members = type.GetMembers();
+
+    foreach (MemberInfo member in members)
+    {
+        WriteLine("{0}: {1} ({2})", member.MemberType, member.Name, member.DeclaringType?.Name);
+        IOrderedEnumerable<CoderAttribute> coders = member
+            .GetCustomAttributes<CoderAttribute>()
+            .OrderByDescending(c => c.LastModified);
+
+        foreach (CoderAttribute coder in coders)
+        {
+            WriteLine("-> Modified by {0} on {1}", coder.Coder, coder.LastModified.ToShortDateString());
+        }
+    }
+}
