da3982ab77804846041cf862b56df1bb416b4703
Ayane Suarez Romero <Ayane.Suarez@external.wolterskluwer.com>
2025-10-12 15:47:18 +0200

    Working with source code generators

--- Chapter06/GeneratingCodeLib/MessageSourceGenerator.cs
+++ Chapter06/GeneratingCodeLib/MessageSourceGenerator.cs
@@ -1,9 +1,31 @@
-﻿using System;
+﻿using Microsoft.CodeAnalysis;
 
-namespace GeneratingCodeLib
+namespace GeneratingCodeLib;
+
+[Generator]
+public class MessageSourceGenerator : ISourceGenerator
 {
-    public class MessageSourceGenerator
+    public void Execute(GeneratorExecutionContext context)
     {
+        IMethodSymbol mainMethod = context.Compilation
+            .GetEntryPoint(context.CancellationToken);
 
+        string typeName = mainMethod.ContainingType.Name;
+        string sourceCode =
+            $@"
+            public static partial class {typeName}
+            {{
+                static partial void Message(string message)
+                {{
+                    System.Console.WriteLine($""Generator says: '{{message}}'"");
+                }}
+            }}";
+
+        // Good Practice: Include .g. or .generated. in the filename of source-generated files.
+        context.AddSource($"{typeName}.Methods.g.cs", sourceCode);
+    }
+
+    public void Initialize(GeneratorInitializationContext context)
+    {
     }
 }
