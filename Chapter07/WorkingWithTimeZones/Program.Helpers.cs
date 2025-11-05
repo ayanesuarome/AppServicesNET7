719ac6f1ecfc7aba75074add2af1d14089722439
Ayane Suarez Romero <Ayane.Suarez@external.wolterskluwer.com>
2025-11-05 22:14:47 +0100

    Working with TimeZones

---
+++ Chapter07/WorkingWithTimeZones/Program.Helpers.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.ObjectModel;
+
+namespace WorkingWithTimeZones;
+
+internal partial class Program
+{
+    static void SectionTitle(string title)
+    {
+        ConsoleColor previousColor = ForegroundColor;
+        ForegroundColor = ConsoleColor.DarkYellow;
+        WriteLine("*");
+        WriteLine($"* {title}");
+        WriteLine("*");
+        ForegroundColor = previousColor;
+    }
+
+    static void OutputTimeZones()
+    {
+        // get the time zones registered with the OS
+        ReadOnlyCollection<TimeZoneInfo> zones = TimeZoneInfo.GetSystemTimeZones();
+        WriteLine("*");
+        WriteLine($"* {zones.Count} time zones:");
+        WriteLine("*");
+
+        // order the time zones by Id instead of DisplayName
+        foreach (TimeZoneInfo zone in zones.OrderBy(z => z.Id))
+        {
+            WriteLine($"{zone.Id}");
+        }
+    }
+
+    static void OutputDateTime(DateTime dateTime, string title)
+    {
+        SectionTitle(title);
+        WriteLine($"Value: {dateTime}");
+        WriteLine($"Kind: {dateTime.Kind}");
+        WriteLine($"IsDaylightSavingTime: {dateTime.IsDaylightSavingTime()}");
+        WriteLine($"ToLocalTime(): {dateTime.ToLocalTime()}");
+        WriteLine($"ToUniversalTime(): {dateTime.ToUniversalTime()}");
+    }
+    static void OutputTimeZone(TimeZoneInfo zone, string title)
+    {
+        SectionTitle(title);
+        WriteLine($"Id: {zone.Id}");
+        WriteLine("IsDaylightSavingTime(DateTime.Now): {0}",
+        zone.IsDaylightSavingTime(DateTime.Now));
+        WriteLine($"StandardName: {zone.StandardName}");
+        WriteLine($"DaylightName: {zone.DaylightName}");
+        WriteLine($"BaseUtcOffset: {zone.BaseUtcOffset}");
+    }
+
+    /// <summary>
+    /// Time zone names change if Daylight Saving time is active.
+    /// e.g. GMT Standard Time becomes GMT Summer Time.
+    /// </summary>
+    static string GetCurrentZoneName(TimeZoneInfo zone, DateTime when) =>
+        zone.IsDaylightSavingTime(when) ?
+            zone.DaylightName :
+            zone.StandardName;
+}
